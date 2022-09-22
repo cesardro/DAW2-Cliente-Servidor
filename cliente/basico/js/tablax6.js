a571044f5f6406b7ad7c867e42a4e88433f4dcec
Cesar Alejandro <cesardro.98@gmail.com>
2022-09-22 10:42:08 +0200

    Tabla de multiplicar x N.

--- cliente/basico/js/tablax6.js
+++ cliente/basico/js/tablax6.js
@@ -1,5 +1,10 @@
-console.log("Tabla del 6");
+let n = prompt("Ingresa número: ")
 
-for (let index = 0; index <= 10; index++) {
-    console.log("6 x " + index + " = " + index * 6);
+if (n > 0 && n < 11) {
+    console.log("Tabla del ", n);
+    for (let index = 0; index <= 10; index++) {
+        console.log(n, " x " + index + " = " + index * n);
+    }
+} else {
+    console.log("Número no válido.")
 }
