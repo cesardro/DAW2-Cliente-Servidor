921bf4e0190aea5c71eb5c9ab1eeda0cc8e106d5
Cesar Alejandro <cesardro.98@gmail.com>
2022-09-22 08:34:19 +0200

    Agregamos código para mostrar los tipos de datos.

--- servidor/formulario/formulario.py
+++ servidor/formulario/formulario.py
@@ -1,8 +1,19 @@
 #!C:\Users\zx21student278\AppData\Local\Microsoft\WindowsApps\python
 
-print("Content-Type: text/html\n")
+print("Content-Type: text/plain\n")
 
 import cgi
 
 args = cgi.parse()
-print(args)
+
+print(args['nombre'][0]) #Imprime el dato que devuelve del formulario.
+print(args['edad'][0])
+
+print(type(args)) #Imprime el tipo de dato que devuelve del formulario.
+print(type(args['edad'])) #Listado, "un array".
+print(type(args['edad'][0])) #Posición 0, un string.
+
+edad = int(args['edad'][0]) 
+
+print(edad) #Imprime la edad convertida de sting a integer.
+print(type(edad)) #Integer.
