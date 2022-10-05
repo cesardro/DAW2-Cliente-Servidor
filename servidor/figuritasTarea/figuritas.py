038d83b4cb65231ebfeed6af5fc7f0082b5d1d69
Cesar Alejandro <112773974+cesardro@users.noreply.github.com>
2022-10-05 13:34:11 +0200

    Figuras tarea

---
+++ servidor/figuritasTarea/figuritas.py
@@ -0,0 +1,40 @@
+#!C:\Users\zx21student278\AppData\Local\Microsoft\WindowsApps\python
+
+import cgi
+print("Content-Type: text/plain\n")
+
+args = cgi.parse()
+nfilas = int(args["nfilas"][0])
+figura = int(args["figura"][0])
+
+def astericos(n):
+    print("Asteriscos\n")
+    for i in range(n):
+        print(' '*(n-i-1) + '* '*(i+1))
+    for i in range(n):
+        print(' '*(i+1) + '* '*(n-i-1))
+
+def numeros(n):
+    print("Números\n")
+    for i in range(n):
+        print(' '*(n-i-1) + (str(i+1)+' ')*(i+1))
+    for i in range(n):
+        print(' '*(i+1) + (str((n-i-2)+1)+' ')*(n-i-1))
+
+def abc(n):
+    print("ABC\n")
+    listaABC = ["A","B","C","D","E","F","G","H","I","J","K","L","M","Ñ","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+
+    for i in range(n):
+        print(' '*(n-i-1) + (chr(65+i)+' ')*(i+1))
+    for i in range(n):
+        print(' '*(i+1) + (chr(65+n-i-2)+' ')*(n-i-1))
+
+if(figura == 1):
+    astericos(nfilas)
+elif(figura == 2):
+    numeros(nfilas)
+elif(figura == 3):
+    abc(nfilas)
+else:
+    print("Opción no contemplada")
