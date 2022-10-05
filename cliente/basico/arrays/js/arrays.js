16550d2269fbdaae129d1784e0f38fa12d63a996
Cesar Alejandro <112773974+cesardro@users.noreply.github.com>
2022-10-05 09:18:32 +0200

    arrays

--- cliente/basico/arrays/js/arrays.js
+++ cliente/basico/arrays/js/arrays.js
@@ -1,22 +1,61 @@
+onload = iniciar;
+
 //Texto - Variable de ambito global
-let texto = ""
+let texto = "";
+
+let datoUsuario;
+let salida;
+let numero;
+
+function iniciar() {
+    //Recuperar objetos del DOM (elementos o nodos)
+    datoUsuario = document.getElementById('valores');
+    salida = document.getElementById('salida');
+    numero = document.getElementById('numero');
+}
 
 function arraysBasico() {
-    //Recuperar objetos del DOM (elementos o nodos)
-    let datoUsuario = document.getElementById('valores');
-    let salida = document.getElementById('salida');
 
-    let listaNumeros = datoUsuario.value.split(" ");
+    let listaNumeros = datoUsuario.value.split(' ');
+
+    console.log(listaNumeros);
 
     listaNumeros[10] = "daw";
 
-    for(num of listaNumeros){
+    listaNumeros.forEach(suma);
+
+    //Imprime todos los elementos inclusive si hay undefined. 
+    for (num of listaNumeros) {
         texto += "a" + num + '<br>';
     }
 
     salida.innerHTML = texto;
 }
 
+//Imprime a traves de for each, se salta los undefined.
 function suma(value) {
-    texto = texto + (parseInt(value) + 2) + "<br>";
+    texto += (parseInt(value) + 2) + "<br>";
+}
+
+//Imprime string.
+function arrayToString() {
+    salida.innerHTML = datoUsuario.value.split(" ").toString();
+}
+
+//Join separa los elementos por lo introducido.
+function arrayJoin() {
+    salida.innerHTML = datoUsuario.value.split(" ").join(numero.value);
+}
+
+//Pop siempre quita el último.
+function arrayPop() {
+    let lista = datoUsuario.value.split(" ");
+    lista.pop();
+    salida.innerHTML = lista.join("-");
+}
+
+function arrayPush() {
+    let lista = datoUsuario.value.split(" ");
+    lista.push(numero.value);
+    salida.innerHTML = lista.join("-");
 }
