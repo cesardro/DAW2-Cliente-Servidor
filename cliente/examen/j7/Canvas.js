c34065a2a4528bb36e42d67a0259cd444187f16d
Cesar Alejandro <112773974+cesardro@users.noreply.github.com>
2022-11-23 08:30:06 +0100

    Carpetas

--- cliente/examen/j7/Canvas.js
+++
@@ -1,8 +0,0 @@
-function cambiaFondo(id) {
-    document.getElementById('p1').style.backgroundColor = "white";
-    document.getElementById('p2').style.backgroundColor = "white";
-    document.getElementById('p3').style.backgroundColor = "white";
-    document.getElementById(id).style.backgroundColor = "lightblue";
-}
-
- 
