4f5fb41def5b4b1c03a22dea1cd6a22ef93b3ab7
Cesar Alejandro <112773974+cesardro@users.noreply.github.com>
2022-12-21 09:09:46 +0100

    Clase

--- cliente/segundoTrimestre/ejercicios/dragAndDrop/juegoDragAndDrop/js/juego.js
+++ cliente/segundoTrimestre/ejercicios/dragAndDrop/juegoDragAndDrop/js/juego.js
@@ -3,24 +3,34 @@ $(principal);
 function principal() {
     const t = $("#tablero");
 
-    const letras = "acb";
+    const letras = "turi";
 
     for (let i = 0; i < letras.length; i++) {
         t.append(
             "<span id='arrastrable" + i + "' draggable='true' ondragend='arrastrando(event)'>" + letras[i] + "</span>"
         );
     }
+    comprobar();
 }
 
 function arrastrando(event) {
     $("#tablero").append($("#" + event.target.id));
     event.dataTransfer.clearData();
+    comprobar();
 }
 
 function comprobar() {
+    let bandera = true;
     let palabra = $("#tablero").text();
     for (let i = 1; i < palabra.length; i++) {
-        if (palabra[i] < palabra[i - 1]) return alert("No esta ordenado");
+        if (palabra[i] < palabra[i - 1] && bandera) {
+            bandera = false;
+        }
     }
-    return alert("Esta ordenado");
+
+    if (bandera) {
+        $("#tablero").css("color", "green");
+    } else {
+        $("#tablero").css("color", "red");
+    }
 }
